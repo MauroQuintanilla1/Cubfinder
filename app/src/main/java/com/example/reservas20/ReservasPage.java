76e5704c1803923b0ec21777bc156843f73eca23
Conversores <70458039a@gmail.com>
2024-04-26 09:50:29 -0600

    Cubfinder

--- app/src/main/java/com/example/reservas20/ReservasPage.java
+++ app/src/main/java/com/example/reservas20/ReservasPage.java
@@ -4,13 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ReservasPage extends AppCompatActivity {
+
+    Button btnreservar, btnreportar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reservas_page);
+
+        btnreservar = findViewById(R.id.btn_reservar);
+        btnreportar = findViewById(R.id.btn_reportar);
+
+        btnreservar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ReservasPage.this, ReglamentoPage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btnreportar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ReservasPage.this, ReglamentoPage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+
     }
 
     @Override
